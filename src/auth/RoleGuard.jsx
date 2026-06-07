8754679dac8b1f0c8a56895b14159e8a898d30e9
Fasakin Halimah <halimahfasakin@gmail.com>
2026-06-07 21:10:03 +0100

    Update frontend API configuration

--- src/auth/RoleGuard.jsx
+++ src/auth/RoleGuard.jsx
@@ -1,6 +1,6 @@
 import { jwtDecode } from 'jwt-decode';
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 const RoleGuard = ({ role }) => {
